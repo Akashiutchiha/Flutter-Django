cf5f6ebb3ba71176b84030290e2a702213b16925
Akashiutchiha <christianhonore2003@gmail.com>
2023-04-17 01:23:55 +0100

    feat: Added the CRUD operations to the django DB

--- Django/notes/api/views.py
+++ Django/notes/api/views.py
@@ -42,3 +42,39 @@ def getRoute(request):
 
 ]
     return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Notes.objects.all()
+    serializer = NotesSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Notes.objects.get(id=pk)
+    serializer = NotesSerializer(notes, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Notes.objects.create(
+        body=data['body'],
+    )
+    serializer = NotesSerializer(note, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Notes.objects.get(id=pk)
+    serializer = NotesSerializer(note, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Notes.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted')
